80fbd7e7333e73f205bae9338c9a4f898a5348fc
olgaaktas <binpolyglotte@gmail.com>
2021-06-26 18:47:42 +0300

    refactor

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,29 @@
 // Validation 
-interface Validatable{
+interface Validatable {
     value: string | number,
-    required?: boolean,
-    minLength?: number,
-    maxLength?: number,
-    min?: number,
-    max?: number
+        required ? : boolean,
+        minLength ? : number,
+        maxLength ? : number,
+        min ? : number,
+        max ? : number
 }
 
-function validate(input: Validatable): boolean{
+function validate(input: Validatable): boolean {
     let isValid = true;
-    if(input.required){
+    if (input.required) {
         isValid = isValid && input.value.toString().trim().length !== 0
     }
-    if(input.minLength != null && typeof input.value === "string"){
+    if (input.minLength != null && typeof input.value === "string") {
         isValid = isValid && input.value.trim().length >= input.minLength
     }
-    if(input.maxLength != null && typeof input.value === "string"){
+    if (input.maxLength != null && typeof input.value === "string") {
         isValid = isValid && input.value.trim().length <= input.maxLength
     }
-    if(input.min != null && typeof input.value === "number"){
+    if (input.min != null && typeof input.value === "number") {
         isValid = isValid && input.value >= input.min
-        console.log(isValid,  input.value, input.min)
+        console.log(isValid, input.value, input.min)
     }
-    if(input.max != null && typeof input.value === "number"){
+    if (input.max != null && typeof input.value === "number") {
         isValid = isValid && input.value <= input.max
     }
     return isValid;
@@ -31,11 +31,11 @@ function validate(input: Validatable): boolean{
 
 
 // autobind decorator
-function autobind(_: any, _2: string, descriptor: PropertyDescriptor){
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
-    const adjDescriptor : PropertyDescriptor = {
+    const adjDescriptor: PropertyDescriptor = {
         configurable: true,
-        get(){
+        get() {
             const boundFn = originalMethod.bind(this);
             return boundFn
         }
@@ -54,7 +54,7 @@ class ProjectInput {
     descriptionInputElement: HTMLInputElement;
     peopleInputElement: HTMLInputElement;
 
-    constructor(){
+    constructor() {
         this.templateElement = document.getElementById("project-input") as HTMLTemplateElement;
         this.hostElement = document.getElementById("app") as HTMLDivElement;
 
@@ -70,44 +70,61 @@ class ProjectInput {
         this.attach();
     }
 
-    private gatherUserInput() : [string, string, number] | void {
-        const enteredTitle =  this.titleInputElement.value;
-        const enteredDescription =  this.descriptionInputElement.value;
-        const enteredPeople =  this.peopleInputElement.value;
-        if(validate({value: enteredTitle, required: true, minLength: 4, maxLength: 20}) &&
-            validate({value: enteredDescription, required: true, minLength: 4, maxLength: 100}) &&
-            validate({value: +enteredPeople, required: true, min: 1, max: 5})) {
-                return [enteredTitle, enteredDescription, +enteredPeople];
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true,
+            minLength: 4,
+            maxLength: 20
+        };
+        const descValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 4,
+            maxLength: 100
         }
-        else {
+        const peopleValidatable: Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5
+        }
+
+        if (validate(titleValidatable) && validate(descValidatable) && validate(peopleValidatable)) {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        } else {
             alert("not valid");
             return;
         }
     }
 
 
-    private clearInputs(){
+    private clearInputs() {
         this.titleInputElement.value = "";
         this.descriptionInputElement.value = "";
         this.peopleInputElement.value = "";
     }
 
     @autobind
-    private sumbitHandler(event: Event){
+    private sumbitHandler(event: Event) {
         event.preventDefault();
         const userInput = this.gatherUserInput();
-        if(Array.isArray(userInput)){
+        if (Array.isArray(userInput)) {
             const [title, desc, people] = userInput;
-            console.log(title,desc, people);
+            console.log(title, desc, people);
             this.clearInputs();
-        }   
+        }
     }
 
-    private configure(){
+    private configure() {
         this.element.addEventListener("submit", this.sumbitHandler);
     }
 
-    private attach(){
+    private attach() {
         this.hostElement.insertAdjacentElement("afterbegin", this.element);
     }
 }
