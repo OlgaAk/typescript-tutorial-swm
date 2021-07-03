34f279df957848955898c1d5d9e3e5db0f9234c9
olgaaktas <binpolyglotte@gmail.com>
2021-07-03 16:45:27 +0300

    folders added

---
+++ project1/src/component/project-list.ts
@@ -0,0 +1,81 @@
+/// <reference path="base-component.ts"/>
+/// <reference path="../model/drag-drop.ts"/>
+
+// ProjectList Class
+namespace App {
+  export class ProjectList
+    extends Component<HTMLDivElement, HTMLElement>
+    implements DropTarget
+  {
+    assignedProjects: Project[];
+
+    constructor(private type: "active" | "finished") {
+      super("project-list", "app", false, `${type}-projects`);
+
+      this.assignedProjects = [];
+
+      this.configure();
+
+      this.renderContent();
+    }
+    @autobind
+    dragOverHandler(event: DragEvent): void {
+      if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
+        event.preventDefault();
+        const listEl = this.element.querySelector("ul")!;
+        listEl.classList.add("droppable");
+      }
+    }
+    @autobind
+    dropHandler(event: DragEvent): void {
+      const listEl = this.element.querySelector("ul")!;
+      listEl.classList.remove("droppable");
+      const id = event.dataTransfer!.getData("text"); // dataTransef data not shown in browser consoles
+      projectState.changeProjectStatus(
+        id,
+        this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished
+      );
+    }
+    @autobind
+    dragLeaveHandler(_: DragEvent): void {
+      const listEl = this.element.querySelector("ul")!;
+      listEl.classList.remove("droppable");
+    }
+
+    configure() {
+      projectState.addListener((projects: any[]) => {
+        const relevantProjects = projects.filter((p) => {
+          if (this.type === "active") {
+            return p.status === ProjectStatus.Active;
+          }
+          return p.status === ProjectStatus.Finished;
+        });
+        this.assignedProjects = relevantProjects;
+        this.renderProjectList();
+      });
+
+      this.element.addEventListener("dragover", this.dragOverHandler);
+      this.element.addEventListener("dragleave", this.dragLeaveHandler);
+      this.element.addEventListener("drop", this.dropHandler);
+    }
+
+    renderContent() {
+      this.element.querySelector(
+        "h2"
+      )!.textContent = `${this.type.toUpperCase()} PROJECTS`;
+
+      const listId = `${this.type}-projects-list`;
+      this.element.querySelector("ul")!.id = listId;
+    }
+
+    renderProjectList() {
+      const list = document.getElementById(
+        `${this.type}-projects-list`
+      ) as HTMLUListElement;
+      list.innerHTML = "";
+      for (const project of this.assignedProjects) {
+        new ProjectItem(this.element.querySelector("ul")!.id, project);
+      }
+    }
+  }
+}
---
+++ project1/src/state/project-state.ts
@@ -0,0 +1,56 @@
+// Project State Management
+namespace App {
+  type Listener<T> = (projects: T[]) => void;
+
+  class BaseState<T> {
+    protected listeners: Listener<T>[] = [];
+
+    addListener(listenerFn: Listener<T>) {
+      this.listeners.push(listenerFn);
+    }
+  }
+
+  export class ProjectState extends BaseState<Project> {
+    private projects: Project[] = [];
+    private static instance: ProjectState;
+
+    private constructor() {
+      super();
+    }
+
+    static getInstance() {
+      if (!this.instance) {
+        this.instance = new ProjectState();
+      }
+      return this.instance;
+    }
+
+    addProject(title: string, description: string, people: number) {
+      const newProject = new Project(
+        Math.random().toString(),
+        title,
+        description,
+        people,
+        ProjectStatus.Active
+      );
+      this.projects.push(newProject);
+      this.notifyListeners();
+    }
+
+    notifyListeners(): void {
+      for (const listener of this.listeners) {
+        listener(this.projects.slice());
+      }
+    }
+
+    changeProjectStatus(projectId: string, status: ProjectStatus): void {
+      const project = this.projects.find((p) => p.id === projectId);
+      if (project && project.status != status) {
+        project.status = status;
+        this.notifyListeners();
+      }
+    }
+  }
+
+  export const projectState = ProjectState.getInstance();
+}
